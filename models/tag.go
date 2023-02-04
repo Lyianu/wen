27ec295a087e0a371521efccf67a659455985853
Lyianu <1066184520@qq.com>
2023-02-04 20:04:00 +0800

    implement GetTags

--- models/tag.go
+++ models/tag.go
@@ -10,3 +10,16 @@ type Tag struct {
 	ModifiedBy string `json:"modified_by"`
 	State      int    `json:"state"`
 }
+
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
+	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
+
+	return
+}
+
+func GetTagTotal(maps interface{}) int {
+	var count64 int64
+	db.Model(&Tag{}).Where(maps).Count(&count64)
+
+	return int(count64)
+}
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,9 +1,42 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Lyianu/wen/models"
+	"github.com/Lyianu/wen/pkg/e"
+	"github.com/Lyianu/wen/pkg/setting"
+	"github.com/Lyianu/wen/util"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+)
 
 func GetTags(c *gin.Context) {
+	name := c.Query("name")
 
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+
+	if name != "" {
+		maps["name"] = name
+	}
+
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+
+	code := e.SUCCESS
+
+	data["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 func AddTag(c *gin.Context) {
