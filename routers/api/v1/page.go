e495e52c062e79cc39e0c7f47a34eb76bb864c03
Lyianu <1066184520@qq.com>
2023-02-07 11:37:58 +0800

    implement DeleteArticle router

--- routers/api/v1/page.go
+++ routers/api/v1/page.go
@@ -104,5 +104,23 @@ func EditPage(c *gin.Context) {
 }
 
 func DeletePage(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID must be positive")
 
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if models.ExistPageByID(id) {
+			code = e.SUCCESS
+			models.DeletePage(id)
+		} else {
+			code = e.ERROR_NOT_EXIST_PAGE
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]interface{}),
+	})
 }
