5bbfe3312b180d2f636380656e3f2779abe816e0
Lyianu <1066184520@qq.com>
2023-02-12 13:06:28 +0800

    add sitesetting

--- wen-frontend/src/Admin.js
+++ wen-frontend/src/Admin.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import SiteSetting from "./components/SiteSetting";
 import Writer from "./components/Writer";
 
 const Admin = () => {
@@ -19,8 +20,21 @@ const Admin = () => {
             <div className="top-con pt-24 pb-8 px-16 border-b justify-items-stretch">
                 <button className='rounded-full border p-3 hover:bg-black hover:text-white' onClick={() => setMode("write")}>Write</button>
                 <button className='rounded-full border p-3 hover:bg-black hover:text-white' onClick={() => setMode("articles")}>Articles</button>
+                <button className='rounded-full border p-3 hover:bg-black hover:text-white' onClick={() => setMode("site")}>Site</button>
             </div>
-            { mode === "write" && <Writer /> }
+            { mode === "write" && (
+                <>
+                    <h1 className="text-2xl px-10 pt-3">Add a post</h1>
+                    <Writer />
+                </>
+            )}
+
+            { mode === "site" && (
+                <>
+                    <h1 className="text-2xl px-10 pt-3">Site settings</h1>
+                    <SiteSetting />
+                </>
+            )}
         </div>
      );
 }
---
+++ wen-frontend/src/components/SiteSetting.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import useFetch from "../useFetch";
+
+const SiteSetting = () => {
+    const [image_url, setImage_url] = useState("")
+    const [name, setName] = useState("")
+    const [bg_title, setBg_title] = useState("")
+    const [desc, setDesc] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(image_url, name, bg_title, desc)
+    }
+
+    const {data, isPending} = useFetch("http://localhost:8000/api/v1/site")
+    useEffect(() => {
+        if (data) {
+            setBg_title(data.bg_title);
+            setImage_url(data.image_url);
+            setName(data.name);
+            setDesc(data.desc);
+        }
+    }, [data])
+
+    return ( 
+        <div className="site-setting">
+            <form className="flex flex-col h-screen" onSubmit={ handleSubmit }>
+                <div className="site-name mx-auto mt-auto p-3">
+                    <label>Name</label>
+                    <input 
+                        className="border m-1 p-1"
+                        type="text"
+                        required
+                        value={ name }
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="desc mx-auto p-3">
+                    <label>Description</label>
+                    <input
+                        className="border m-1 p-1"
+                        type="text"
+                        value={ desc }
+                        onChange={(e) => setDesc(e.target.value)}
+                    />
+                </div>
+                <div className="bg-title mx-auto p-3">
+                    <label>Background Title</label>
+                    <input
+                        className="border m-1 p-1"
+                        type="text"
+                        value={ bg_title }
+                        onChange={(e) => setBg_title(bg_title)}
+                    />
+                </div>
+                <div className="bg-image mx-auto p-3">
+                    <label>Background Image URL</label>
+                    <input
+                        className="border m-1 p-1"
+                        type="text"
+                        value={ image_url }
+                        onChange={(e) => setImage_url(image_url)}
+                    />
+                </div>
+
+                <button className="rounded-full border p-3 m-auto px-10 hover:bg-black hover:text-white">Submit</button>
+            </form>
+        </div>
+     );
+}
+ 
+export default SiteSetting;
