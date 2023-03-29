16321b550cb53f6bed598d19e1ff63cd3e1d58a2
ast <1066184520@qq.com>
2023-03-29 11:54:30 +0800

    change hero style

--- wen-frontend/src/components/Hero.js
+++ wen-frontend/src/components/Hero.js
@@ -4,11 +4,11 @@ const Hero = ({ title_text, image_url, is_pending, desc_text }) => {
     return ( 
         <div className="hero py-32 bg-slate-600 bg-cover bg-center relative -z-10" style={ { backgroundImage: url, height: "50%" } }>
                 <div className="title justify-center flex">
-                    <h1 className="text-5xl">{ is_pending ? "..." : title_text }</h1>
+                    <h1 className="text-5xl text-white">{ is_pending ? "..." : title_text }</h1>
                 </div>
                 {desc_text && (
-                    <div className="desc justify-start flex px-8 italic absolute bottom-10">
-                        <h2 className="text-3xl">{ desc_text }</h2>
+                    <div className="desc justify-start flex px-8 italic absolute bottom-8">
+                        <h2 className="text-xl">{ desc_text }</h2>
                     </div>
                 )}
         </div>
